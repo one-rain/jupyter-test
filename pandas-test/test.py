19b82c03ea386ba6ca2437605ac544d3c01d8cbf
one-rain <hellowangrun@163.com>
2020-09-20 12:08:21 +0800

    update

---
+++ pandas-test/test.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+print('hello world')
+
+dates = pd.date_range('20130101', periods=6)
+print(dates)
+
+df2 = pd.DataFrame({'A': 1., 
+                    'B': pd.Timestamp('20130102'), 
+                    'C': pd.Series(1, index=list(range(4)), dtype='float32'), 
+                    'D': np.array([3] * 4, dtype='int32'), 
+                    'E': pd.Categorical(["test", "train", "test", "train"]), 
+                    'F': 'foo'})
+            
+print(df2)
