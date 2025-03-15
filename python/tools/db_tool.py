422729944ef8432c7a0d6247aa76a8434d81c399
one-rain <hellowangrun@163.com>
2025-03-15 22:44:38 +0800

    update

---
+++ python/tools/db_tool.py
@@ -0,0 +1,29 @@
+import pymysql
+from pyhive import presto
+
+
+def mysql_connect():
+    '''
+    MySQL连接
+    '''
+    return pymysql.connect(
+        host = 'localhost',
+        user = 'dev',
+        password = '342024',
+        database = 'test',
+        charset = 'utf8mb4'
+    )
+
+
+def mysql_connect():
+    '''
+    Presto连接
+    '''
+    presto_conn = presto.connect(
+        host = 'emr-bi',
+        port = 9090,
+        catalog='hive',
+        schema='default'
+    )
+
+    return presto_conn.cursor()
